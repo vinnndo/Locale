103b889095295ae1fe52b1567e82d7d6d4607e4f
AhmadShkour <73717039+AhmadShkour71@users.noreply.github.com>
2022-03-30 23:37:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-const languages = {};
-languages.ar = require('./ar-SA/website.json');
-languages.es = require('./es-ES/website.json');
-
-module.exports = languages;
-module.exports.webLangs = {
-  ar: { name: "العربية", flag: "sa" },
-  en: { name: "English", flag: "us" },
-};
-
-
 const botLanguages = {};
 botLanguages.ar = require('./ar-SA/bot.json');
 botLanguages.es = require('./es-ES/bot.json');
