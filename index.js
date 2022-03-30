25eb670141639a9916245d02f0a7e1c3d448c6a0
AhmadShkour <73717039+AhmadShkour71@users.noreply.github.com>
2022-03-30 23:39:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const botLanguages = {};
-botLanguages.ar = require('./ar-SA/bot.json');
+botLanguages.ar = require('./ar-JO/bot.json');
 botLanguages.es = require('./es-ES/bot.json');
 
 module.exports.botLangs = botLanguages;
-
-module.exports.ex = require('./examples.json');
